dcfa94779f27660dfb856a43ec7d0da2cf13d658
ad-victoriam-101 <keeper24@gmail.com>
2019-06-18 15:43:09 -0700

    first commit

---
+++ python_test.py
@@ -0,0 +1,25 @@
+#python test quesions
+# given an array of numbers find the missing one. 
+import random
+test_array = []
+def fill_array():
+    #generates a new array of 1-100
+    for number in range(101):
+        test_array.append(number)
+def array_pop():
+    #generates a random index and pops it out of test_array
+    for x in range(1):
+        random_int = random.randint(1,101)
+        print(random_int)
+        test_array.pop(random_int)
+def missing_number(test_arrays):
+    #this is a guassian formula that is implemented in python to find the missing number. 
+    n = len(test_arrays)
+    total = ((n)*(n+1))/2
+    sum_of_array = sum(test_arrays)
+    missing = total - sum_of_array
+    print("the missing number is:",missing)
+fill_array()
+array_pop()
+print(test_array)
+missing_number(test_array)
