7f7b3f9bba9467b9f75b7ca9c98f5ddcd35dc720
ad-victoriam-101 <keeper24@gmail.com>
2019-06-30 15:06:36 -0700

    first push of binary trees

---
+++ Binary_Tree_Questions/intro_binary_trees.py
@@ -0,0 +1,10 @@
+#this is just an intro into python binary trees. 
+class Node:
+    def __init__(self,key):
+        self.left = None
+        self.right = None
+        self.val = key
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
