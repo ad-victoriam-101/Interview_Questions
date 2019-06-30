faaf6055ec8375d60f54a79916aa0a665afb5a2d
ad-victoriam-101 <keeper24@gmail.com>
2019-06-30 13:51:44 -0700

    solved reverse string

--- String_questions/reverse_string.py
+++ String_questions/reverse_string.py
@@ -1,2 +1,5 @@
 #in this exercise take a given string and reverse it returning the new string and theold one. 
 test_string = "the quick brown fox jumped over the lazy dog"
+def return_reverse_string(string):
+    return "".join(reversed(string))
+print return_reverse_string(test_string)
