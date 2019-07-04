8ad61582ed82232e306a6cd9138e3aac22d27480
ad-victoriam-101 <keeper24@gmail.com>
2019-07-03 17:05:53 -0700

    first push bubble sort

---
+++ Misc_Coding_Questions/bubble_sort.py
@@ -0,0 +1,18 @@
+#given an array, implement bubble sort. 
+#This is a very slow sorting algorithm compared to algorithms like quicksort, with worst-case complexity O(n^2). 
+#However, the tradeoff is that bubble sort is one of the easiest sorting algorithms to implement from scratch.
+import random
+test_array = []
+for i in range(101):
+    test_array.append(random.randint(1,101))
+print(test_array)
+
+def bubble_sort(test_list):
+    for passnum in range(len(test_list)-1,0,1):
+        for i in range(passnum):
+            if test_list[i]>test_list[i+1]:
+                temp = test_list[i]
+                test_list[i] = test_list[i+1]
+                test_list[i+1] = temp
+bubble_sort(test_array)
+print(test_array)
