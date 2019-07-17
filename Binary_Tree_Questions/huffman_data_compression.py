6d792fa2da3bcd158bf1746612674b589c6f1963
ad-victoriam-101 <keeper24@gmail.com>
2019-07-17 15:17:01 -0700

    second block of code, node generation

--- Binary_Tree_Questions/huffman_data_compression.py
+++ Binary_Tree_Questions/huffman_data_compression.py
@@ -12,10 +12,22 @@ print("Your data is " , len_of_str*8 , "bits long")
 #Create a list of characters and their frequency 
 letters = []
 only_letters = []
-for letters  in input_string:
+for letter in input_string:
     if letter not in letters:
         freq = input_string.count(letter)
         letters.append(freq)
         letters.append(letter)
         only_letters.append(letter)
+#print(letters)
+#print(only_letters)
+# from this we have the frequency of each letter used we also remove duplicate letters to make it easier to read later. 
 
+#after generating this data we can now start to build out our tree with nodes. 
+
+nodes = []
+while len(letters) > 0:
+    nodes.append(letters[0:2])
+    letters = letters[2:]
+nodes.sort()
+huffman_tree = []
+huffman_tree.append(nodes)
