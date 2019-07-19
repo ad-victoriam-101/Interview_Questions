db938b62e050ff5f8d75492cf7a7a630d42ca714
ad-victoriam-101 <keeper24@gmail.com>
2019-07-19 16:25:34 -0700

    fixed times number

--- Binary_Tree_Questions/huffman_data_compression.py
+++ Binary_Tree_Questions/huffman_data_compression.py
@@ -6,7 +6,7 @@ print("*"*8)
 input_string = input("Enter a string you wish to compress:")
 len_of_str = len(input_string)
 print("Your Message is: " , (input_string))
-print("Your data is " , len_of_str*8 , "bits long")
+print("Your data is " , len_of_str*7 , "bits long")
 #Gathers user input and saves them to arguments. also informs user of how the data looks. uncompressed.
 
 ################################################################################################################################
