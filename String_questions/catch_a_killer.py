e9e47e9180e738c3a2018a5be7ac847ef964003c
ad-victoriam-101 <keeper24@gmail.com>
2019-07-23 14:43:30 -0700

    added first string question

---
+++ String_questions/catch_a_killer.py
@@ -0,0 +1,7 @@
+import string
+
+alphabet = list(string.ascii_lowercase)
+
+input_string = "O eqf’z rg ziol qfndgkt. It ktyxltl zg wtsotct qfnziofu ol vkgfu"
+input_string.split()
+print (input_string)
