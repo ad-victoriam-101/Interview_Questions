f4446ad737ed9df48aa787105b6e56f085983ef9
ad-victoriam-101 <keeper24@gmail.com>
2019-07-23 16:03:33 -0700

    added first push of new file>

---
+++ Array_Questions/Mega_corp_salary.py
@@ -0,0 +1,3 @@
+# Given an array representing a line of seats of employees at MegaCorp, determine how much each one should get paid.
+# For example, given [10, 40, 200, 1000, 60, 30], you should return [1, 2, 3, 4, 2, 1].
+f
