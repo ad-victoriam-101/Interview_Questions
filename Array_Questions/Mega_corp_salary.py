2a9f3c3e22ff8c7c6b76174be22e16fe25f567b7
ad-victoriam-101 <keeper24@gmail.com>
2019-07-23 16:24:59 -0700

    finished adding online code

--- Array_Questions/Mega_corp_salary.py
+++ Array_Questions/Mega_corp_salary.py
@@ -7,11 +7,11 @@
 #else take the +1 as the new number and continue while the array still has numbers to test. 
 def get_segments(arr):
     #make function that takes an argument
-    asc = arr[1]>arr[0]
+    asc = arr[1] > arr[0]
     prev = arr[0]
     start = 0
     segments = []
-    for i in num in enumerate(arr[1:]):
+    for i, num in enumerate(arr[1:]):
         if (asc and num<prev) or (not asc and num > prev):
             segments.append((asc,i-start +1))
             start = i + 1
@@ -19,3 +19,23 @@ def get_segments(arr):
         prev = num
     segments.append((asc , len(arr)-start))
     return segments
+
+def get_bonuses(arr):
+    if not arr:
+        return []
+    if len(arr) == 1:
+        return[1]
+    segments = get_segments(arr)
+    bonuses = list()
+    for segment in segments:
+        asc, length = segment
+        seg_bonuses = list(range(length))
+        if not asc:
+            seg_bonuses.reverse()
+        bonuses.extend(seg_bonuses)
+    bonuses = [x+1 for x in bonuses]
+    print(bonuses)
+    return bonuses
+    
+assert get_bonuses([1000])
+assert get_bonuses([10,40,200,1000,60,30])
