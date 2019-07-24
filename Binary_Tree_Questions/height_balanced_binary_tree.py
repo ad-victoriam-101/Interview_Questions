c604f040118feff589454bcb282d8c072469f6ff
ad-victoriam-101 <keeper24@gmail.com>
2019-07-24 14:53:44 -0700

    first push

---
+++ Binary_Tree_Questions/height_balanced_binary_tree.py
@@ -0,0 +1,6 @@
+#given a binary tree, determin whether or not its a height - balanced. 
+#this can be defined as one in which the heights of the two subtrees of any node never differ by more than one.
+
+#step one: make binary tree with N nodes.
+#step two: count the number of nodes/branches on each side. 
+#step three: if this number is the same return true.else return false. 
