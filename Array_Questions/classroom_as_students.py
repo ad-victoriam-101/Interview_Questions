ce3df3c850b0d4ec032c474198c40e7c158449f9
ad-victoriam-101 <keeper24@gmail.com>
2019-07-31 15:32:00 -0700

    took a solution ofline, had issues with the i looping though

--- Array_Questions/classroom_as_students.py
+++ Array_Questions/classroom_as_students.py
@@ -1,7 +1,58 @@
 #given a classrom of N students, whose friendships can be represented in an adjacency list. for example the following describes a situation where 
 #0 is friends with 1,2,3 and 6 
-data_driver = {0:[1,2],1:[0,5],2:[0],3:[6],4:[],5:[1],6:[3],7:[]}
-print(len(data_driver))
-for i in range(len(data_driver)):
-    print(data_driver[i])
-    
+import random
+
+
+def populate_group(node, adj_list, group):
+    group.add(node)
+
+    adj_nodes = adj_list[node]
+    if not adj_nodes:
+        return
+
+    for anode in adj_nodes:
+        if anode not in group:
+            populate_group(anode, adj_list, group)
+
+
+def get_groups(nodes, adj_list, groups):
+    num_nodes = len(nodes)
+    while num_nodes:
+        new_group = set()
+        node = list(nodes)[0]
+        populate_group(node, adj_list, new_group)
+        groups.append(new_group)
+        nodes -= new_group
+        num_nodes = len(nodes)
+
+    return groups
+
+
+def get_num_groups(nodes, adj_list):
+    groups = list()
+    isolated = set()
+    for node in nodes:
+        if not adj_list[node]:
+            isolated.add(node)
+
+    for node in isolated:
+        groups.append({node})
+        nodes.remove(node)
+        del adj_list[node]
+
+    groups = get_groups(nodes, adj_list, groups)
+    print(groups)
+    return len(groups)
+
+
+# Tests
+adj_list = {
+    0: [1, 2],
+    1: [0, 5],
+    2: [0],
+    3: [6],
+    4: [],
+    5: [1],
+    6: [3]
+}
+assert get_num_groups(set(range(7)), adj_list) == 3
