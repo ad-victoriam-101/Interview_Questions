7d9480358b63e1e8f474dbc4ceef508d24b2183b
ad-victoriam-101 <keeper24@gmail.com>
2019-07-31 14:45:32 -0700

    test works, finished code.

--- Binary_Tree_Questions/all_possible_BTS.py
+++ Binary_Tree_Questions/all_possible_BTS.py
@@ -3,3 +3,13 @@
 #remember each node can only have two offsprings. 
 #the solution for now many nodes you can have is a corelation formula called Catalan formula, 
 
+def Catalan_formula(number_nodes):
+    if number_nodes <= 1:
+        return 1
+    res = 0
+    for i in range(number_nodes):
+        res += Catalan_formula(i) * Catalan_formula(number_nodes - i -1)
+    return res
+
+for i in range(10):
+    print(Catalan_formula(i))
