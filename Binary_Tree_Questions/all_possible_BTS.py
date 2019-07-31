61126afdb5ae7ff812c740578cdfcfbd21ce3b47
ad-victoriam-101 <keeper24@gmail.com>
2019-07-31 14:41:35 -0700

    first push, added rules.

---
+++ Binary_Tree_Questions/all_possible_BTS.py
@@ -0,0 +1,5 @@
+#given n'th nodes in a Binary search tree. 
+#find all unique itterations of the tree. 
+#remember each node can only have two offsprings. 
+#the solution for now many nodes you can have is a corelation formula called Catalan formula, 
+
