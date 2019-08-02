ce40afdc391394c5f6dc0dd3af32bf784d5cd0bf
ad-victoriam-101 <keeper24@gmail.com>
2019-08-02 09:43:56 -0600

    added comments

--- Misc_Coding_Questions/recursion_fib.py
+++ Misc_Coding_Questions/recursion_fib.py
@@ -2,13 +2,19 @@
 FibArray = [0,1] 
   
 def fibonacci(n): 
-    
+    #test sequence makes sure the test is only preformed on proper inputs.
+
     if n<0: 
         print("Incorrect input") 
     elif n<=len(FibArray): 
+        #same idea you don't need to actually calculate the sum of 2 numbers as they are already set.
+
         return FibArray[n-1] 
     else: 
+        #if the input passes all our checks we then can start calculating the sequence. 
+
         temp_fib = fibonacci(n-1)+fibonacci(n-2) 
+        
         FibArray.append(temp_fib) 
         return temp_fib 
   
