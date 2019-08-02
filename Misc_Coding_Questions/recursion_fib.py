7d72f64472162f01b882689de8ecb0b1276c71c4
ad-victoriam-101 <keeper24@gmail.com>
2019-08-02 09:39:35 -0600

    finished code.

--- Misc_Coding_Questions/recursion_fib.py
+++ Misc_Coding_Questions/recursion_fib.py
@@ -1,3 +1,17 @@
 #write a firbinachy sequence that takes a user input and then generates all sequences to that point. 
-def factorial(start_num):
+FibArray = [0,1] 
+  
+def fibonacci(n): 
     
+    if n<0: 
+        print("Incorrect input") 
+    elif n<=len(FibArray): 
+        return FibArray[n-1] 
+    else: 
+        temp_fib = fibonacci(n-1)+fibonacci(n-2) 
+        FibArray.append(temp_fib) 
+        return temp_fib 
+  
+# Driver Program 
+  
+print(fibonacci(21))
