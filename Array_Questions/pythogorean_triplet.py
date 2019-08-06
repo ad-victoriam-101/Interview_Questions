5427860cff87266e76ae1ee8543437d2ab00bc29
ad-victoriam-101 <keeper24@gmail.com>
2019-08-06 10:16:01 -0700

    first push with some psudeo code.

---
+++ Array_Questions/pythogorean_triplet.py
@@ -0,0 +1,9 @@
+#given an array of numbers check if any of them follow the pythogorean code of a^2 + b^2 = c^2
+test_array = [2,3,4,5,6,6,7,8,8,9,10]
+
+def triplet_test(array):
+    for i in range(len(array)):
+        if i**2 + array[i+1]**2 == array[i+2]**2:
+            print("the flollowing numbers are triplets:",i,array[i+1],array[i+2])
+
+triplet_test(test_array)
