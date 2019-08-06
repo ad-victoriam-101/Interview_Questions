332dfe223177aceddad7d607275f54146c10c335
ad-victoriam-101 <keeper24@gmail.com>
2019-08-06 13:34:22 -0700

    added working solution for the problem.

--- Array_Questions/height_building_285.py
+++ Array_Questions/height_building_285.py
@@ -4,8 +4,20 @@
 
 # Can you do this using just one forward pass through the array?
 #look at which of the numbers is the highest. then go to the right side and take all buildings that are lower than it. then check that each building is greater than its value to the right. 
-#if a number is larger than the number checked minus the number of buildings that have sun light. 
+#if a number is larger than the number checked minus the number of buildings that have sun light.
+# 
+test_array=[3,7,8,3,6,1] 
+# remember that 8 is the largest num and the start of what we care about. 
 def buildings_with_sunlight(arr):
-    for i in range(len(arr)):
-        
+    buldings_list = list()
+    #creates an empty list
+    for height in arr:
+        if buldings_list and buldings_list[-1] < height:
+            buldings_list.pop()
+        buldings_list.append(height)
+    print(buldings_list)
+    return buldings_list
 
+
+        
+buildings_with_sunlight(test_array)
