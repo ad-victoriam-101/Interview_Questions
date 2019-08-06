3d86097c82ab5c507d1d5a9ae750be7b4a6c293d
ad-victoriam-101 <keeper24@gmail.com>
2019-08-06 10:32:52 -0700

    added working ish code.

--- Array_Questions/pythogorean_triplet.py
+++ Array_Questions/pythogorean_triplet.py
@@ -1,9 +1,14 @@
 #given an array of numbers check if any of them follow the pythogorean code of a^2 + b^2 = c^2
 test_array = [2,3,4,5,6,6,7,8,8,9,10,12,13,5,12,13]
 
-def triplet_test(array):
-    for i in range(len(array)-2):
-        if array[i]**2 + array[i+1]**2 == array[i+2]**2:
-            print("the following numbers are triplets:",array[i],array[i+1],array[i+2])
+def pythogorean_trip(arr):
+    num_squared = [x * x for x in arr]
+    set_of_squares = set(num_squared)
+    for i in range(len(num_squared)-1):
+        for k in range(i+1,len(num_squared)-1):
+            summed = num_squared[i] + num_squared[k]
+            if summed in set_of_squares:
+                print(i,k,summed,": are triplets.")
+                return True
+        return False
 
-triplet_test(test_array)
