0e712db89ee1e48730c4dad25f4ecb3cee301d52
ad-victoriam-101 <keeper24@gmail.com>
2019-08-05 17:18:36 -0700

    finished code. have to check up more onthat formula as i wanna know more.

--- Array_Questions/regular_number.py
+++ Array_Questions/regular_number.py
@@ -1,2 +1,22 @@
 #a regular number in mathematics is defined as one which evenly divides some power of 60.
 #given an integer N write a program that returns in order, the first N regular numbers. 
+def is_hamming_nums(x):
+    if x ==1 :
+        return 1
+    if x % 2 == 0:
+        return is_hamming_nums(x/2)
+    if x % 3 == 0:
+        return is_hamming_nums(x/3)
+    if x % 5 == 0 :
+        return is_hamming_nums(x/5)
+    return 0
+
+def hamming_sequence(x):
+    if x == 1:
+        return 1
+    hamming_sequence(x-1)
+    if is_hamming_nums(x) == True:
+        print("%s" % x)
+print(is_hamming_nums(7))
+print(is_hamming_nums(1))
+hamming_sequence(24)
