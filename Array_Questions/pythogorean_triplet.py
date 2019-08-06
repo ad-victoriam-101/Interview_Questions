ef002e28532fc03913d0c2b3526fc806788e6ae3
ad-victoriam-101 <keeper24@gmail.com>
2019-08-06 10:23:58 -0700

    adding code to preform function out of order.

--- Array_Questions/pythogorean_triplet.py
+++ Array_Questions/pythogorean_triplet.py
@@ -1,9 +1,9 @@
 #given an array of numbers check if any of them follow the pythogorean code of a^2 + b^2 = c^2
-test_array = [2,3,4,5,6,6,7,8,8,9,10]
+test_array = [2,3,4,5,6,6,7,8,8,9,10,12,13,5,12,13]
 
 def triplet_test(array):
-    for i in range(len(array)):
-        if i**2 + array[i+1]**2 == array[i+2]**2:
-            print("the flollowing numbers are triplets:",i,array[i+1],array[i+2])
+    for i in range(len(array)-2):
+        if array[i]**2 + array[i+1]**2 == array[i+2]**2:
+            print("the following numbers are triplets:",array[i],array[i+1],array[i+2])
 
 triplet_test(test_array)
