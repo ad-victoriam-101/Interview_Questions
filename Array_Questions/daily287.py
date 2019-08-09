e85513fb6c5bdb6d9d395f19e84c8d59b830ffac
ad-victoriam-101 <keeper24@gmail.com>
2019-08-09 09:30:23 -0700

    adding more comments

--- Array_Questions/daily287.py
+++ Array_Questions/daily287.py
@@ -15,17 +15,23 @@ def get_similar_websites(visits, k=1):
     #k is the number of pairs, so it will pick the sites that have the most connections. 
 
     website_users = dict()
-    #
+    #creates an empty dictionary
     for website, user in visits:
+        #for loops can take two arguments in this case it grabs the first (website) then user in visit.
         if website not in website_users:
+            #adds the website to the dic if its not in the dic yet
             website_users[website] = set()
+            #this makes a set of all unique users to visit a website.
         website_users[website].add(user)
 
     websites = list(website_users.keys())
+    #makes a list from the keys of the div we created earlier. 
 
     most_similar = list()
     for i in range(len(websites) - 1):
+        #jump though all the keys we created in the website list.
         for j in range(i + 1, len(websites)):
+            #this will take the keys, compare them to the sim score.Co
             web_a, web_b = websites[i], websites[j]
             sim_score = get_similarity_score(website_users[web_a], website_users[web_b])
             heappush(most_similar, (-sim_score, (web_a, web_b)))
