c67988641acaec1fce627e81c08e26be1b46672e
ad-victoriam-101 <keeper24@gmail.com>
2019-08-19 11:26:24 -0700

    first push of initial data.

---
+++ Array_Questions/sock_pairs.py
@@ -0,0 +1,6 @@
+#given an array of ints representing pairs of socks, determine how many pairs of socks you can make. 
+#info given is the length of the arry, N and the array arr = [1,2,1,2,1,3,2]
+arr = [1,2,1,2,3,2]
+n = len(arr)
+
+print(arr, n)
