683017989e9c04c9579ed5179a53226d342b4e39
ad-victoriam-101 <keeper24@gmail.com>
2019-08-19 11:50:33 -0700

    added first section of code and function count_pairs:

--- Array_Questions/sock_pairs.py
+++ Array_Questions/sock_pairs.py
@@ -2,5 +2,27 @@
 #info given is the length of the arry, N and the array arr = [1,2,1,2,1,3,2]
 arr = [1,2,1,2,3,2]
 n = len(arr)
-
+import math as mt 
+from itertools import groupby
+def count_pairs(arr,n):
+    mp = dict()
+    for i in range(n):
+        if arr[i] in mp.keys():
+            mp[arr[i]] += 1
+        else:
+            mp[arr[i]]=1
+    ans = 0
+    for it in mp:
+        count = mp[it]
+        ans += (count * (count-1))//2
+    return ans
+#this is not exactly right, this counts every pair and will count doubles. 
+# print(count_pairs(arr,n))
 print(arr, n)
+count_of_pairs = map(int,raw_input().split())
+
+pairs = 0
+for val in [len(list(group)) for key , group in groupby(sorted(count_of_pairs))]:
+    pairs = pairs + val/2
+return pairs
+
