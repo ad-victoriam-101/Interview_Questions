c4c4b1917d92d29cefa33c8e4e743d99572e26a9
ad-victoriam-101 <keeper24@gmail.com>
2019-08-27 12:27:38 -0700

    first push, added inspection code.

---
+++ Array_Questions/cloud_jumper.py
@@ -0,0 +1,3 @@
+#For each game, Emma will get an array of clouds numbered  if they are safe or  if they must be avoided. 
+# For example, c = [0,1,0,0,0,1,0] indexed from 0 - 6. The number on each cloud is its index in the list so she must avoid the clouds at indexes  and . 
+# She could follow the following two paths:  or . The first path takes jumps while the second takes .
