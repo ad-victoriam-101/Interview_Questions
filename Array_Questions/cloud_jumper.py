5a9289177c86c59660b732f53cfb89157f5c49e2
ad-victoriam-101 <keeper24@gmail.com>
2019-08-27 13:33:47 -0700

    added solution

--- Array_Questions/cloud_jumper.py
+++ Array_Questions/cloud_jumper.py
@@ -1,3 +1,15 @@
 #For each game, Emma will get an array of clouds numbered  if they are safe or  if they must be avoided. 
 # For example, c = [0,1,0,0,0,1,0] indexed from 0 - 6. The number on each cloud is its index in the list so she must avoid the clouds at indexes  and . 
 # She could follow the following two paths:  or . The first path takes jumps while the second takes .
+clouds = [0,1,0,0,0,1,0]
+def cloud_jumper(arr):
+    jumps, i = 0,0
+    while i < len(arr)-1:
+        # if 2 cloud jumps
+        if i+2 < len(arr) and arr[i+2] != 1:
+            i += 1
+        jumps += 1
+        i += 1
+    return jumps
+
+print(cloud_jumper(clouds))
