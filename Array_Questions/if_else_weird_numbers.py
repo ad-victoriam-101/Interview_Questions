a8d4df34f00ccf390b2abda3babfedfad7c108bd
ad-victoriam-101 <keeper24@gmail.com>
2019-09-01 12:47:36 -0700

    added if else weird program.

---
+++ Array_Questions/if_else_weird_numbers.py
@@ -0,0 +1,20 @@
+# Given an integer, , perform the following conditional actions:
+# If  is odd, print Weird
+# If  is even and in the inclusive range of 2 to ,5 print Not Weird
+# If  is even and in the inclusive range of 6 to 20, print Weird
+# If  is even and greater than 20 , print Not Weird
+
+def if_odd_weird(n):
+    if n%2==0:
+        if n in range(2,5):
+            print("Not Weird")
+        if n in range(6,20):
+            print("Weird")
+        if n > 20:
+            print("Not Weird")
+    else:
+        print('weird')
+        
+if_odd_weird(3)
+if_odd_weird(16)
+if_odd_weird(33)
