ceeea0f46d052bf115d2cebed3244881d5824240
ad-victoriam-101 <keeper24@gmail.com>
2019-09-06 14:46:50 -0700

    added working file

--- Array_Questions/coin_change.py
+++ Array_Questions/coin_change.py
@@ -7,6 +7,23 @@
 # So the output should be 5.
 #values can be reused. 
 N = 10
-S = {2,5,3,6}
-def coin_counter(coin,values):
-    
+S = [2,5,3,6]
+def coin_counter(S,m,n):
+
+    # table[i] will be storing the number of solutions for 
+    # value i. We need n+1 rows as the table is constructed 
+    # in bottom up manner using the base case (n = 0) 
+    # Initialize all table values as 0 
+    table = [0 for k in range(n+1)]
+    table[0] = 1
+    # print(table)
+    # Pick all coins one by one and update the table[] values 
+    # after the index greater than or equal to the value of the 
+    # picked coin 
+    for i in range (0,m):
+        for j in range(S[i],n+1):
+            table[j] += table[j-S[i]]
+            print(table)
+    return table[n]
+
+print(coin_counter(S,len(S),N))
