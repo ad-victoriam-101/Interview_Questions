a32dfe2490ac8bdc281c03fa009246c5cea9bca6
ad-victoriam-101 <keeper24@gmail.com>
2019-09-12 11:42:33 -0700

    added working solution.

--- Array_Questions/jump_to_N.py
+++ Array_Questions/jump_to_N.py
@@ -3,3 +3,21 @@
 # On the ith jump, you may move exactly i places to the left or right.
 
 # Find a path with the fewest number of jumps required to get from 0 to N.
+n = int(input("please enter a number: "))
+
+def jump_to_target(num):
+    abs_num = abs(num)
+    if abs_num < 2:
+        return abs_num
+
+    point, new_point = None, 0
+    jump = 1
+    while new_point <= abs_num:
+        point = new_point
+        new_point += jump
+        jump += 1
+    jump -= 2
+    return (2 * (abs_num - point)) + jump
+
+print(jump_to_target(n))
+        
