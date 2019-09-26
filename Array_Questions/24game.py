b2be5c163f7acca6227067139cd539f6b2b9ebce
ad-victoriam-101 <keeper24@gmail.com>
2019-09-26 15:43:19 -0700

    added intro and rules.

---
+++ Array_Questions/24game.py
@@ -0,0 +1,3 @@
+# given an array of 4 integers each between 1-9,
+# in a fixed order. try to add, subtract, multiply or divide these numbers in a way to add up to 24. 
+# You can use parenteses to section off the number. 
