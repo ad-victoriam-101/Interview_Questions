74c7c7606eb3b3743a974e0b2abce6880c2d863b
ad-victoriam-101 <keeper24@gmail.com>
2019-10-08 10:52:23 -0700

    added more code and inorder taversal.

--- Binary_Tree_Questions/ternary_search_word_tree.py
+++ Binary_Tree_Questions/ternary_search_word_tree.py
@@ -10,11 +10,42 @@
 # |    / |   | \ 
 # x   b  e   r  e  
 
-given_arr = ["code","cod","cob","bee","ax","war","we"]
-print(given_arr)
+parent = ["code","cod","cob","bee","ax","war","we"]
+# print(given_arr)
 
 class Node:
     def __init__(self,key):
         self.left = None
         self.right = None
         self.val = key
+def createNode(parent,i,created,root):
+    if created[i] is not None:
+        return
+    created[i] = Node(i)
+    if parent[i] == -1:
+        root[0] = created[i]
+        return
+    if created[parent[i]] is None:
+        createNode(parent,parent[i])
+    p = created[parent[i]]
+    if p.left is None:
+        p.left = created[i]
+    else:
+        p.right = created[i]
+
+def createTree(parent):
+    n = len(parent)
+    created = [None for i in range(n+1)]
+    root = [None]
+    for i in range(n):
+        createNode(parent,i,created,root)
+    return root[0]
+def inorder(root):
+    if root is not None:
+        inorder(root.left)
+        print (root.key)
+        inorder(root.right)
+
+root = createTree(parent)
+
+inorder(root)
