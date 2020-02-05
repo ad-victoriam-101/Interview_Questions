284e1a24ec4442a302d564ef6cb2538fa8b510ad
ad-victoriam-101 <keeper24@gmail.com>
2020-02-05 13:13:14 -0800

    -m add

--- Dictionary_Questions/Dictionary_and_Structuring.py
+++ Dictionary_Questions/Dictionary_and_Structuring.py
@@ -1,2 +1,12 @@
 #Getting the data out right. 
 #Dictionary data type, is a collection of many values. Usually stored in key value pairs. mycat = {'size':'FAT','Colour':'Gray','disposition':'loud'}
+import pprint
+my_cat = {'size':'FAT','Colour':'Gray','disposition':'loud'}
+print(my_cat['size'])
+
+message ='it was a bright cold day in april, and the clocks were strickign thirteen.'
+count = {}
+for char in message:
+    count.setdefault(char,0)
+    count[char] = count[char]+1
+pprint.pprint (count)
