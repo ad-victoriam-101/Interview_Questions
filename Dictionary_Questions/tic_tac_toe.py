c0325757ac0444de55b49a68159abba8bbaec559
ad-victoriam-101 <keeper24@gmail.com>
2020-02-10 18:11:22 -0800

    -madded

--- Dictionary_Questions/tic_tac_toe.py
+++ Dictionary_Questions/tic_tac_toe.py
@@ -1,5 +1,6 @@
 theBoard = {'top-L':'','top-M':'','top-R':'','mid-L':'','mid-M':'','mid-R':'','low-L':'','low-M':'','low-R':''}
 print(theBoard)
 
-def printBoard(board):
-    print()
+#code for an empty dictionary is. {} or dict()
+#y = {'foo':42}
+#list is a collection of values, dictionary is a collection of keys and values.
